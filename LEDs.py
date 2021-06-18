b8b2d0696665e6dbcc723fb106560ff908a3ba2d
TheSillySmoker <thesillysmoker@gmail.com>
2021-06-18 23:13:28 +1000

    leds

---
+++ LEDs.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+import time
+ledPin = 21
+GPIO.setmode(GPIO.BMC)
+GPIO.setup(ledPin, GPIO.OUT)
+
+x = 1
+while x == 1:
+    GPIO.output(ledPin, GPIO.HIGH)
+    time.sleep(0.1)
+    GPIO.output(ledPin, GPIO.LOW)
+    time.sleep(0.1)
